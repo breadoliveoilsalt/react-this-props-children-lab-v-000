eda091e6b4f170d69871897e375f4741ef1a56e6
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-05-25 10:00:12 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,30 @@
 // Code Invitation Component Here
+//
+// import React from 'react';
+//
+// class Invitation extends React.Component {
+//
+//   render() {
+//     return (
+//       <div>
+//         <h1>You've been invited!</h1>
+//         {this.props.children}
+//       </div>
+//     )
+//   }
+// }
+// export default Invitation;
+
+import React from 'react';
+
+export default class Invitation extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>You've been invited!</h1>
+        {this.props.children}
+      </div>
+    );
+  }
+
+};
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,42 @@
 // Code ThemedDecoration Component Here
+
+
+import React from 'react';
+
+class ThemedDecorations extends React.Component {
+
+  render() {
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => React.cloneElement(child, {
+      className: this.props.theme,
+    }));
+
+    return (
+      <div>
+        {childrenWithExtraProp}
+      </div>
+    );
+  }
+}
+
+export default ThemedDecorations;
+
+// THIS IS WHAT I HAD; WOULD NOT PASS FOR SOME REASON:
+// import React from 'react';
+//
+// export default class ThemedDecorations extends React.Component {
+//   render() {
+//     const childrenWithWrapperDiv = React.Children.map(this.props.children, child => {
+//       React.cloneElement(child, {
+//       className: this.props.theme
+//       // return (
+//       //   <div className={theme}>{child}</div>
+//       // );
+//     })});
+//     return (
+//       <div>
+//         {childrenWithWrapperDiv}
+//       </div>
+//
+//     )
+//   }
+// }
